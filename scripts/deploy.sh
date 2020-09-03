bc79571fdaceba1cf20fb35860b805b1856aad31
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-09-03 20:44:41 +0800

    布置源代码

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,6 @@ git init &&
 git add . &&
 git commit -m 'deploy' &&
 # git remote add origin git@github.com:li123147/money-2-website.git &&
-git remote add origin git@gitee.com:lixiaohu123147/money-3-website.git &&
+git remote add origin git@gitee.com:lixiaohu123147/blog-1.git &&
 git push -u origin master -f
 cd -
